84721eaf3ce813cabab7f7d83d014f037ee75471
Vinicius Dias <carlosv775@gmail.com>
2019-10-14 18:18:19 -0300

    Correcting paypal e-mail template

--- src/Service/PayPalEmailParser.php
+++ src/Service/PayPalEmailParser.php
@@ -20,15 +20,18 @@ class PayPalEmailParser extends EmailParser
         $domDocument->loadHTML($email->textHtml);
         $xPath = new \DOMXPath($domDocument);
 
-        $productQuery = '//div/div/div/blockquote/div/table/tbody/tr/td[2]/table[1]/tbody/tr/td/table/tbody/tr[3]/td/table[2]/tbody/tr/td[2]/div/table[3]/tbody/tr[2]/td[1]';
+        $productQuery = '/html/body/table/tr/td[2]/table[1]/td/table/tr[3]/td/table[2]/tr/td[2]/div/table[3]/tr[2]/td[1]';
         $dataNodes = $xPath->query($productQuery);
         preg_match('/Tv express (mensal|anual)/i', $dataNodes->item(0)->textContent, $matches);
         $product = $matches[1];
 
-        $emailQuery = '//div/div/div/blockquote/div/table/tbody/tr/td[2]/table[1]/tbody/tr/td/table/tbody/tr[3]/td/table[2]/tbody/tr/td[2]/div/span[3]/span/a';
-        $emailAddress = $xPath->query($emailQuery)
-            ->item($dataNodes->length - 1)
-            ->textContent;
+        $emailQuery = '/html/body/table/tr/td[2]/table[1]/td/table/tr[3]/td/table[2]/tr/td[2]/div/span[3]/span';
+        $emailAddress = trim(
+            $xPath->query($emailQuery)
+                ->item($dataNodes->length - 1)
+                ->textContent,
+            ' ()'
+        );
 
         return new Sale(new Email($emailAddress), $product);
     }
